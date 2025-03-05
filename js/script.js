87e3b8059a2b067d0576bd429fb3b93481abf3b2
Kobutaemon <ceti1241010@gn.iwasaki.ac.jp>
2025-03-05 11:28:49 +0900

    お問い合わせのh2を追加した所。CSSの修正はまだ。

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ $(function() {
             var windowHeight = $(window).height(); //ウィンドウの高さ取得
             if (scroll > imgPos - windowHeight + windowHeight/5){ //scroll > imgPosだけだと画面の一番上に来た時にフェードインしてしまうので、遅い。そこで基準となるimgPosの数値を小さくすることで、画面の一番上から少し下に要素が来たときにフェードインするようになる。
                 $(this).addClass("fade-in");
-            } else {
-                $(this).removeClass("fade-in");
+            // } else {
+            //     $(this).removeClass("fade-in");
             }
         });
     });
